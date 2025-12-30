17f519b0b3675d13952f0da9d648ef1065f386a7
wjinss <aastu4343@gmail.com>
2025-12-31 01:25:40 +0900

    learn. 레벨0 '배열 원소의 길이' 풀이 및 방법 비교

---
+++ level0/arrayElementsLength.js
@@ -0,0 +1,56 @@
+// 배열 원소의 길이
+// 문자열 배열 strlist가 매개변수로 주어집니다. strlist 각 원소의 길이를 담은 배열을 return하도록 solution 함수를 완성해주세요.
+
+// 제한사항
+// 1 ≤ strlist 원소의 길이 ≤ 100
+// strlist는 알파벳 소문자, 대문자, 특수문자로 구성되어 있습니다.
+// 입출력 예
+// strlist	result
+// ["We", "are", "the", "world!"]	[2, 3, 3, 6]
+// ["I", "Love", "Programmers."]	[1, 4, 12]
+// 입출력 예 설명
+// 입출력 예 #1
+
+// ["We", "are", "the", "world!"]의 각 원소의 길이인 [2, 3, 3, 6]을 return합니다.
+// 입출력 예 #2
+
+// ["I", "Love", "Programmers."]의 각 원소의 길이인 [1, 4, 12]을 return합니다.
+
+// ------------------------------------------------------------------------------------------
+
+// 나의 풀이
+
+function solution(strlist) {
+  return strlist.map((str) => str.length);
+}
+
+// ------------------------------------------------------------------------------------------
+
+// 다른 사람의 풀이
+
+{
+  function solution(strlist) {
+    return strlist.reduce((a, b) => [...a, b.length], []);
+  }
+  // reduce 사용
+}
+
+{
+  function solution(strlist) {
+    let answer = [];
+    strlist.forEach((el) => answer.push(el.length));
+    return answer;
+  }
+  // forEach 사용
+}
+
+{
+  function solution(strlist) {
+    let answer = [];
+    for (let i = 0; i < strlist.length; i++) {
+      answer.push(strlist[i].length);
+    }
+    return answer;
+  }
+  // 반복문 사용
+}
