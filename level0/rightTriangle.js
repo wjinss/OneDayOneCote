36596a20eaa47be3ef9c9ec5021754ea0abc95cf
wjinss <aastu4343@gmail.com>
2026-03-19 12:33:33 +0900

    learn. 레벨0 '직각삼각형' 출력하기

---
+++ level0/rightTriangle.js
@@ -0,0 +1,86 @@
+// 직각삼각형 출력하기
+// "*"의 높이와 너비를 1이라고 했을 때, "*"을 이용해 직각 이등변 삼각형을 그리려고합니다. 정수 n 이 주어지면 높이와 너비가 n 인 직각 이등변 삼각형을 출력하도록 코드를 작성해보세요.
+
+// 제한사항
+// 1 ≤ n ≤ 10
+// 입출력 예
+// 입력 #1
+
+// 3
+// 출력 #1
+
+// *
+// **
+// ***
+// 입출력 예 설명
+// 입출력 예 #1
+
+// n이 3이므로 첫째 줄에 * 1개, 둘째 줄에 * 2개, 셋째 줄에 * 3개를 출력합니다.
+
+// ------------------------------------------------------------------------------------------
+
+// 나의 풀이
+
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+
+rl.on("line", function (line) {
+  input = line.split(" ");
+}).on("close", function () {
+  const num = Number(input[0]);
+  let Str = "";
+  for (let i = 0; i < num; i++) {
+    for (let j = 0; j <= i; j++) {
+      Str += "*";
+    }
+    Str += "\n";
+  }
+  console.log(Str);
+});
+
+// ------------------------------------------------------------------------------------------
+
+// 다른 사람의 풀이
+{
+  const readline = require("readline");
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  let input = [];
+
+  rl.on("line", function (line) {
+    input = line.split(" ");
+  }).on("close", function () {
+    solution(Number(input[0]));
+  });
+
+  function solution(n) {
+    for (let i = 1; i < n + 1; i++) {
+      console.log("*".repeat(i));
+    }
+  }
+}
+{
+  const readline = require("readline");
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  let input = [];
+
+  rl.on("line", function (line) {
+    input = line.split(" ");
+  }).on("close", function () {
+    for (let i = 1; i <= +input[0]; i++) {
+      console.log("*".repeat(i));
+    }
+  });
+}
